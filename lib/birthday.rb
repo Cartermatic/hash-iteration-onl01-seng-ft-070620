0caf0f84234c0341a73b17986e88b834f5f81c68
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 20:04:50 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,15 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <13
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
+  end
+end
+birthday_kids
+end
 
 # expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
