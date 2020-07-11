45c6eea7dd07388ccae11bf87ebc1b68c4246132
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 20:01:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,18 +6,13 @@
 
 
 def happy_birthday(birthday_kids)
-  birthday_kids = {
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
-}
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
 def age_appropriate_birthday(birthday_age)
-  birthday_kids.each do |kids_name, age|
+  birthday_age.each do |kids_name, age|
     if age <13
       puts "Happy Birthday #{name}! You are now #{age} years old!"
   else
