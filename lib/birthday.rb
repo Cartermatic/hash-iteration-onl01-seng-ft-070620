5d2f79040f9f43d7f06641df607cb4f5a49324ec
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 19:59:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -19,6 +19,7 @@ def age_appropriate_birthday(birthday_age)
     puts "You are too old for this."
   end
 end
-
+birthday_age
+end
 
 # expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
